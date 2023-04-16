73ee7bd06ca857f923fd5df688120d2722c64223
Ruslan <ruslan040506@gmail.com>
2023-04-16 19:39:50 +0200

    Checking for an authorized user

--- views/post-list.php
+++ views/post-list.php
@@ -5,7 +5,13 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("HouseCeeper");
+global $USER;
+if (!$USER->IsAuthorized()){
+	LocalRedirect('/sign-in');
+}
 
-$APPLICATION->IncludeComponent('hc:post.list', '', []);
+$APPLICATION->IncludeComponent('hc:post.list', '', [
+	'housePath' => $_REQUEST['housePath'],
+]);
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
