6d0107812496d97e11bcac44df71820af5df2c41
Ruslan <ruslan040506@gmail.com>
2023-04-16 19:42:07 +0200

    Add house-add page for admin

--- install/routes/houseceeper.php
+++ install/routes/houseceeper.php
@@ -5,9 +5,37 @@ use Bitrix\Main\Routing\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes)
 {
+	//$routes->get('/', new PublicPageController('/local/modules/hc.houseceeper/views/house-list.php'));
+	$routes->get('/', function () {
+		global $USER;
+		if(!$USER->IsAuthorized()) {
+			LocalRedirect('/sign-in');
+		}
+		if($USER->IsAdmin()) {
+			LocalRedirect('/house-list');
+		}
+		\Hc\Houseceeper\Repository\House::redirectToHisHouse($USER->GetID());
+	});
 
-	$routes->get('/', new PublicPageController('/local/modules/hc.houseceeper/views/post-list.php'));
-	$routes->get('/post', new PublicPageController('/local/modules/hc.houseceeper/views/post-details.php'));
+	$routes->get('/logout', function() {
+		Hc\Houseceeper\Controller\Auth::logout();
+	});
+	$routes->post('/login', function () {
+		Hc\Houseceeper\Controller\Auth::signin();
+	});
+	$routes->post('/reg', function()  {
+		Hc\Houseceeper\Controller\Auth::signupUser();
+	});
+
+	$routes->get('/house-list', new PublicPageController('/local/modules/hc.houseceeper/views/house-list.php'));
+	$routes->get('/add-house', new PublicPageController('/local/modules/hc.houseceeper/views/house-add.php'));
+	$routes->post('/add-house', function () {
+		$house = new \Hc\Houseceeper\Controller\House();
+		$house->addNewHouse();
+	});
+
+	$routes->get('/house/{housePath}', new PublicPageController('/local/modules/hc.houseceeper/views/post-list.php'));
+	$routes->get('/house/{$housePath}/post', new PublicPageController('/local/modules/hc.houseceeper/views/post-details.php'));
 	$routes->get('/sign-up', new PublicPageController('/local/modules/hc.houseceeper/views/sign-up.php'));
 	$routes->get('/sign-in', new PublicPageController('/local/modules/hc.houseceeper/views/sign-in.php'));
 };
