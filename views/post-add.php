f3c50a0fee2e85b9dd6c7af8cd2911c3e243b968
VolkovMatvey <matavech@icloud.com>
2023-04-17 23:15:05 +0200

    added simple post-add menu

---
+++ views/post-add.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @var CMain $APPLICATION
+ */
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+$APPLICATION->SetTitle("HouseCeeper");
+global $USER;
+if (!$USER->IsAuthorized()){
+	LocalRedirect('/sign-in');
+}
+
+$APPLICATION->IncludeComponent('hc:post.add', '', [
+	'housePath' => $_REQUEST['housePath'],
+]);
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
