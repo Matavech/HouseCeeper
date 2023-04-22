3289f84a9d8ad768d07876e915d89d546fcd6f4f
VolkovMatvey <matavech@icloud.com>
2023-04-22 15:40:11 +0200

    format Date in comments

--- install/components/hc/comment/class.php
+++ install/components/hc/comment/class.php
@@ -3,6 +3,12 @@
 class CommentComponent extends CBitrixComponent {
 	public function executeComponent()
 	{
+		$this->prepareComponentParams();
 		$this->includeComponentTemplate();
+	}
+
+	protected function prepareComponentParams()
+	{
+		$this->arParams['COMMENT']['DATETIME_CREATED'] = FormatDate('X', $this->arParams['COMMENT']['DATETIME_CREATED']);
 	}
 }
