0f8832ccc5d0988a114bfe182177aecbe7ae688f
VolkovMatvey <matavech@icloud.com>
2023-04-27 21:38:28 +0200

    formatted date

--- install/components/hc/comment/class.php
+++ install/components/hc/comment/class.php
@@ -11,7 +11,10 @@ class CommentComponent extends CBitrixComponent {
 	protected function prepareComponentParams()
 	{
 		global $USER;
-		$this->arParams['COMMENT']['DATETIME_CREATED'] = FormatDate('X', $this->arParams['COMMENT']['DATETIME_CREATED']);
+		$this->arParams['COMMENT']['DATETIME_CREATED'] = FormatDate([ 's' => 'sago',
+																	  'i' => 'iago',
+																	  'H' => 'Hago',
+																	  '' => 'dago',], $this->arParams['COMMENT']['DATETIME_CREATED']);
 		$apartments = \Hc\Houseceeper\Repository\House::getUserApartmentNumber($USER->GetID(), $_REQUEST['housePath']);
 		$loc = count($apartments)>1 ? 'HC_HOUSECEEPER_COMMENT_APARTMENTS' : 'HC_HOUSECEEPER_COMMENT_APARTMENT';
 		$this->arParams['COMMENT']['USER_APARTMENT_NUMBER'] = \Bitrix\Main\Localization\Loc::getMessage($loc);
