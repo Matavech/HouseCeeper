33647f72954ad2a0e4e1dfc48372a9ce13254912
Ruslan <ruslan040506@gmail.com>
2023-04-27 20:46:18 +0200

    User remove and headman add/delete only for admin

--- install/components/hc/apartment.user.list/templates/.default/template.php
+++ install/components/hc/apartment.user.list/templates/.default/template.php
@@ -23,35 +23,37 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 				<td style="width: 220px">
 					<?= $user['FULL_NAME'] ?>
 				</td>
-				<td style="width: 250px">
-					<?php if (\Hc\Houseceeper\Repository\User::isHeadman($user['ID'], $arParams['HOUSE_ID'])) { ?>
-						<form method="post" action="delete-headman">
-							<input type="hidden" name="house-id" value="<?= $arParams['HOUSE_ID'] ?>">
-							<input type="hidden" name="headman-id" value="<?= $user['ID'] ?>">
-							<button type="submit"
-									class="button is-warning "><?= \Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HOUSEDETAILS_WITHDRAW_AUTHORITY') ?>
-							</button>
-						</form>
-					<?php } else { ?>
-						<form method="post" action="add-headman">
+				<?php if ($USER->IsAdmin()) { ?>
+					<td style="width: 250px">
+						<?php if (\Hc\Houseceeper\Repository\User::isHeadman($user['ID'], $arParams['HOUSE_ID'])) { ?>
+							<form method="post" action="delete-headman">
+								<input type="hidden" name="house-id" value="<?= $arParams['HOUSE_ID'] ?>">
+								<input type="hidden" name="headman-id" value="<?= $user['ID'] ?>">
+								<button type="submit"
+										class="button is-warning "><?= \Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HOUSEDETAILS_WITHDRAW_AUTHORITY') ?>
+								</button>
+							</form>
+						<?php } else { ?>
+							<form method="post" action="add-headman">
+								<input type="hidden" name="house-id" value="<?= $arParams['HOUSE_ID'] ?>">
+								<input type="hidden" name="user-id" value="<?= $user['ID'] ?>">
+								<button type="submit"
+										class="button is-primary "><?= \Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HOUSEDETAILS_ADD_HEADMAN') ?>
+								</button>
+							</form>
+						<?php } ?>
+					</td>
+					<td>
+						<form method="post" action="remove-user">
 							<input type="hidden" name="house-id" value="<?= $arParams['HOUSE_ID'] ?>">
 							<input type="hidden" name="user-id" value="<?= $user['ID'] ?>">
+							<input type="hidden" name="apartment-id" value="<?= $arParams['APARTMENT_ID'] ?>">
 							<button type="submit"
-									class="button is-primary "><?= \Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HOUSEDETAILS_ADD_HEADMAN') ?>
+									class="button is-danger "><?= \Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HOUSEDETAILS_REMOVE_USER') ?>
 							</button>
 						</form>
-					<?php } ?>
-				</td>
-				<td>
-					<form method="post" action="remove-user">
-						<input type="hidden" name="house-id" value="<?= $arParams['HOUSE_ID'] ?>">
-						<input type="hidden" name="user-id" value="<?= $user['ID'] ?>">
-						<input type="hidden" name="apartment-id" value="<?= $arParams['APARTMENT_ID'] ?>">
-						<button type="submit"
-								class="button is-danger "><?= \Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HOUSEDETAILS_REMOVE_USER') ?>
-						</button>
-					</form>
-				</td>
+					</td>
+				<?php } ?>
 			</tr>
 		<?php } ?>
 		</tbody>
