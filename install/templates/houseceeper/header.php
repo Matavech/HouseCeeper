ad7d30e35017ee92bee2e7c78ea1ec9b1fded7b5
VolkovMatvey <matavech@icloud.com>
2023-04-30 19:12:54 +0200

    fixed bug #36

--- install/templates/houseceeper/header.php
+++ install/templates/houseceeper/header.php
@@ -35,7 +35,7 @@ $housePath = extractValueFromLink($_SERVER['REQUEST_URI']);
 
 
 	<div id="navbarBasicExample" class="navbar-menu">
-		<?php if ($USER->IsAuthorized() && !$USER->IsAdmin()) { ?>
+		<?php if ($housePath) { ?>
 		<div class="navbar-start">
 			<a class="navbar-item" href="/house/<?= $housePath?>">
 				<?=\Bitrix\Main\Localization\Loc::getMessage('HC_HOUSECEEPER_HEADER_FEED')?>
